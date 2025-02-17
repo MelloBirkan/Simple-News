334baf89af47dcded4f375e0ed8bd1c49286cb3d
Marcello Gonzatto Birkan <mgbirkan@icloud.com>
2025-02-17 18:16:28 -0300

    Initial Commit

---
+++ Simple News/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Simple News
+//
+//  Created by Marcello Gonzatto Birkan on 17/02/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Simple News/Simple_NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Simple_NewsApp.swift
+//  Simple News
+//
+//  Created by Marcello Gonzatto Birkan on 17/02/25.
+//
+
+import SwiftUI
+
+@main
+struct Simple_NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
